d0160c593c4197544a415ef3d4a4348a87f5be44
Fernando Silva <liquuid@gmail.com>
2022-04-18 05:41:46 -0300

    initial zapeden

---
+++ src/zapeden.js
@@ -0,0 +1,155 @@
+class ZapEden {
+    cosmo () {
+        let images = [
+            {"name":"img1", "src":"assets/Cosmo/cosmo_01.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_02.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_03.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_04.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_05.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_06.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_07.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_08.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_09.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_10.webp"},
+            {"name":"img1", "src":"assets/Cosmo/cosmo_11.webp"},
+
+        ];
+        let imgIndex = 0;
+        
+        function changeImg(){
+            
+            if(images.length > imgIndex+1){
+                document.getElementById("slideshow").src = images[imgIndex].src;
+                imgIndex++;
+            } 
+        }
+        
+        changeImg();
+        setInterval(changeImg, 150);
+    }
+
+    elos () {
+        let images = [
+            {"name":"img1", "src":"assets/Elos/elos_01.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_02.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_03.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_04.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_05.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_06.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_07.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_08.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_09.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_10.webp"},
+            {"name":"img1", "src":"assets/Elos/elos_11.webp"},
+
+        ];
+        let imgIndex = 0;
+        
+        function changeImg(){
+            
+            if(images.length > imgIndex+1){
+                document.getElementById("slideshow").src = images[imgIndex].src;
+                imgIndex++;
+            } 
+        }
+        
+        changeImg();
+        setInterval(changeImg, 150);
+    }
+
+    menu () {
+        let images = [
+            {"name":"img1", "src":"assets/Menu/future call_menu_01.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_02.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_03.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_04.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_05.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_06.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_07.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_08.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_09.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_10.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_11.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_12.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_13.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_14.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_15.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_16.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_17.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_18.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_19.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_20.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_21.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_22.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_23.webp"},
+            {"name":"img1", "src":"assets/Menu/future call_menu_24.webp"},
+        ];
+        let imgIndex = 0;
+        
+        function changeImg(){
+            
+            if(images.length > imgIndex+1){
+                document.getElementById("slideshow").src = images[imgIndex].src;
+                imgIndex++;
+            } 
+        }
+        
+        changeImg();
+        setInterval(changeImg, 150);
+    }
+    self () {
+        let images = [
+            {"name":"img1", "src":"assets/Self/self_01.webp"},
+            {"name":"img1", "src":"assets/Self/self_02.webp"},
+            {"name":"img1", "src":"assets/Self/self_03.webp"},
+            {"name":"img1", "src":"assets/Self/self_04.webp"},
+            {"name":"img1", "src":"assets/Self/self_05.webp"},
+            {"name":"img1", "src":"assets/Self/self_06.webp"},
+            {"name":"img1", "src":"assets/Self/self_07.webp"},
+            {"name":"img1", "src":"assets/Self/self_08.webp"},
+            {"name":"img1", "src":"assets/Self/self_09.webp"},
+            {"name":"img1", "src":"assets/Self/self_10.webp"},
+            {"name":"img1", "src":"assets/Self/self_11.webp"},
+            
+        ];
+        let imgIndex = 0;
+        
+        function changeImg(){
+            
+            if(images.length > imgIndex+1){
+                document.getElementById("slideshow").src = images[imgIndex].src;
+                imgIndex++;
+            } 
+        }
+        
+        changeImg();
+        setInterval(changeImg, 150);
+    }
+    soma () {
+        let images = [
+            {"name":"img1", "src":"assets/Soma/soma_01.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_02.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_03.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_04.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_05.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_06.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_07.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_08.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_09.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_10.webp"},
+            {"name":"img1", "src":"assets/Soma/soma_11.webp"},
+        ];
+        let imgIndex = 0;
+        
+        function changeImg(){
+            
+            if(images.length > imgIndex+1){
+                document.getElementById("slideshow").src = images[imgIndex].src;
+                imgIndex++;
+            } 
+        }
+        
+        changeImg();
+        setInterval(changeImg, 150);
+    }
+}
